550045b90402f69fbea28dfcb4c8379ebc3dfbb4
Lexicon9 <lexicon99999@gmail.com>
2022-02-19 12:08:35 -0800

    new colors/layout, fully class based components

--- Component.js
+++ Component.js
@@ -1,28 +1,54 @@
 class Component {
-  compName;
-  width;
-  height;
-  outputs;
+  static idCounter = 0;
 
-  constructor(type) {
+  constructor(text, type, width, height, items) {
+    this.id = (Component.idCounter++).toString();
+    this.text = text;
     this.type = type;
+    this.width = width;
+    this.height = height;
+    this.items = items;
   }
 
-  generateHTML() {
-    const component = this.genElemC("div", "component");
+  generateHTML(isNew) {
+    const component = this.genElemIC("div", this.id, "component");
+    component.style.height = this.unitsToPixels(this.height);
+    component.style.width = this.unitsToPixels(this.width);
 
     component.appendChild(this.genElemC("div", "handle"));
-    component.appendChild(document.createTextNode("test"));
-    component.appendChild(this.genElemA("input", "type", this.type));
-
-    const zone = this.genElemC("div", "dropzone draggable-dropzone--occupied");
-    zone.appendChild(component);
-
-    document.getElementById("componentMenu").appendChild(zone);
+    if (this.items)
+      this.items.forEach(item => {
+        component.appendChild(item.generate());
+      });
+    // component.appendChild(this.genElemH("div", "&ensp;" + this.text));
+    // component.appendChild(this.genElemA("input", "type", this.type));
+    if (isNew) {
+      const zone = document.getElementById("drop" + this.id);
+      zone.classList.add("draggable-dropzone--occupied");
+      console.log("isNew", component, zone)
+      zone.appendChild(component);
+    } else {
+      const zone = this.genElemIC(
+        "div",
+        "drop" + this.id,
+        "dropzone menu-zone draggable-dropzone--occupied"
+      );
+      zone.style.height = this.unitsToPixels(this.height);
+      zone.appendChild(component);
+      document.getElementById("componentMenu").appendChild(zone);
+    }
   }
 
   genElemC(tag, className) {
     const elem = document.createElement(tag);
+    elem.className = className;
+    return elem;
+  }
+
+  genElemIC(tag, idName, className) {
+    const elem = document.createElement(tag);
+    elem.attValue;
+    elem.id = idName;
     elem.className = className;
     return elem;
   }
@@ -34,18 +60,68 @@ class Component {
     return elem;
   }
 
+  genElemH(tag, innerHTML) {
+    const elem = document.createElement(tag);
+    elem.innerHTML = innerHTML;
+    return elem;
+  }
+
   genElemA(tag, attrName, attValue) {
     const elem = document.createElement(tag);
     elem.setAttribute(attrName, attValue);
     return elem;
   }
+
+  unitsToPixels(units) {
+    return 54 + (units - 1) * 59 + "px";
+  }
+
+  static findById(id, componentArray) {
+    let component = componentArray.find(component => component.id == id);
+    return component;
+  }
 }
 
-class Input {
-  type;
+class Item {
+  constructor(x = 0, y = 0, width = 0, height = 10, tag = "div", content = "hah", extraStyle = "") {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.tag = tag;
+    this.content = content;
+    this.extraStyle = extraStyle || " " + extraStyle;
+  }
+
+  generate() {
+    const elem = document.createElement(this.tag);
+    elem.setAttribute(
+      "style",
+      `position: absolute; width: ${this.x}px; height: ${this.height}px; left: ${this.x}px; top: ${this.y}px;${this.extraStyle}`
+    );
+    elem.innerHTML = this.content;
+    return elem;
+  }
 }
 
-// const check = Symbol(bool);
+class Input extends Item {
+  constructor(x, y, width, height, type = "text", content, extraStyle) {
+    super(x, y, width, height, undefined, content, extraStyle);
+    this.type = type;
+  }
+
+  generate() {
+    const elem = document.createElement("input");
+    elem.setAttribute(
+      "style",
+      `position: absolute; width: ${this.x}px; height: ${this.height}px; left: ${this.x}px; top: ${this.y}px; ${this.extraStyle}`
+    );
+    elem.setAttribute("type", this.type);
+    // elem.setAttribute()
+    return elem;
+  }
+}
+
 // const intBox = Symbol(int);
 // const strBox = Symbol(str);
 // const dropdown = Symbol(enume);
--- script.js
+++ script.js
@@ -1,17 +1,59 @@
 function main() {
-  // components = new Array()
-  // array.forEach(element => {
-  //   element
-  // });
+  // generate dropzones
+  for (let i = 0; i < 17 * 22; i++) {
+    const zone = document.createElement("div");
+    zone.className = "dropzone sheet-zone";
+    document.getElementById("sheetMain").appendChild(zone);
+  }
+  /* todo:
+   * connect to db
+   * instantiate array of sheet components from db
+   * instantiate array of menu components from db
+   * generate html
+   */
+  /*
+  fetch("1b-database.php", { method: "POST" })
+    .then(res => res.text())
+    .then(txt => {
+      document.getElementById("demo").innerHTML = txt;
+    });
+  //*/
 
-  const test = new Component("dropdown");
-  test.generateHTML();
+  // db = jdbc.connect();
+  // sheetComponents = db.load
+  // sheetComponents.forEach(element => element.generateSheet(db.sheet.components.element));
+  // menuComponents = db.load
+  // menuComponents.forEach(element => element.generateMenu(db.components.element));
+  menuComponents = [
+    new Component("Character Name", "text", 5, 2, [
+      new Item(20, 40, 10, 30, "div", "button", "border: 1px double indianred;"),
+      new Input(20, 0, 10, 30, "button", "?", "background-color: firebrick; border: 3px bevel indianred;"),
+    ]),
+    new Component("background hue", "color", 2, 10, [new Item(), new Item()]),
+    new Component("inspiration", "checkbox", 4, 3, [new Item(), new Item()]),
+    new Component("roll damage", "button", 15, 2, [new Item(), new Item()]),
+    new Component("date", "select", 7, 22, [new Item(), new Item()]),
+  ];
+  menuComponents.forEach(element => element.generateHTML());
 
   const droppable = new Draggable.Droppable(document.querySelectorAll(".container"), {
     dropzone: ".dropzone",
     draggable: ".component",
     handle: ".handle",
+    //delay: 100,
   });
 
-  droppable.on("drag:dropped", () => console.log("dropped"));
+  // todo: on drag begin, instead create new html with the same
+  // shrunken mirror but a full size version filling dropzones
+  droppable.on("drag:start", evt => {
+    console.log(evt);
+    if (evt.data.sourceContainer.id == "componentMenu") {
+      Component.findById(evt.data.source.id, menuComponents).generateHTML(1);
+    }
+  });
+
+  // todo: on drop switch to
+  droppable.on("droppable:stop", evt => {
+    console.log(evt);
+  });
 }
